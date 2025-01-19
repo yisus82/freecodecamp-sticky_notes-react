0f53a4197b39f50f3abab63323c417297d02d024
yisus82 <djalma8@gmail.com>
2025-01-19 21:53:44 +0100

    Add loading notes indicator

--- sticky-notes/src/pages/NotesPage.tsx
+++ sticky-notes/src/pages/NotesPage.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
 import { db } from '../appwrite/databases';
 import NoteCard from '../components/NoteCard';
+import Spinner from '../icons/Spinner';
 import { DBNote } from '../types/app';
 
 const NotesPage = () => {
   const [activeNoteCardId, setActiveNoteCardId] = useState('');
   const [notes, setNotes] = useState<DBNote[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const init = async () => {
+    setIsLoading(true);
     const response = await db.notes.list();
     const dbNotes = response.documents as DBNote[];
     setNotes(dbNotes);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -21,6 +25,15 @@ const NotesPage = () => {
     await db.notes.delete(id);
     setNotes(prevState => prevState.filter(note => note.$id !== id));
   };
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <Spinner size={32} />
+        <span>Loading notes...</span>
+      </div>
+    );
+  }
 
   return (
     <div>
